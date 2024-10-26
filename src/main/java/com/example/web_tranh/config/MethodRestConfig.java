f53feb545dc874c070f4f11050e241621dcfe42d
Nguyen Van Do Phi <dophi04062002@gmail.com>
2024-10-26 12:00:00 +0700

    Commit

---
+++ src/main/java/com/example/web_tranh/config/MethodRestConfig.java
@@ -0,0 +1,31 @@
+package com.example.web_tranh.config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MethodRestConfig implements RepositoryRestConfigurer {
+    @Autowired
+    private EntityManager entityManager;
+
+    private String url = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+
+    }
+
+    private void blockHttpMethods(Class c, RepositoryRestConfiguration config, HttpMethod[] methods) {
+        config.getExposureConfiguration()
+                .forDomainType(c)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(methods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(methods));
+    }
+}
